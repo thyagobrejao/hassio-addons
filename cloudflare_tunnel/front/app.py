0fbd819bbacad3e837a346c210b355d28fbf41ab
Thyago Assunção <thyago.brejao@gmail.com>
2022-06-03 15:37:28 -0300

    Trying to write to /config 2

--- cloudflare_tunnel/front/app.py
+++ cloudflare_tunnel/front/app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
+import os
+
+path = '/config/djtec'
+
+# Check whether the specified path exists or not
+isExist = os.path.exists(path)
+
+if not isExist:
+
+  # Create a new directory because it does not exist
+  os.makedirs(path)
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    file = open('/config/djtec/cacilda.txt', 'a+')
+    file = open(path + '/cacilda.txt', 'a+')
     file.write("Woops! I have deleted the content!")
     file.close()
     return render_template('hello.html')
